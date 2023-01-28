a16a6e1e57532cca72337ab4ad3e26eddaf4f87e
Tamara DBre <breversemail@gmail.com>
2023-01-28 19:55:59 +0000

    pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define the root for the website
+  root 'pages#home'
 
+  get 'WhoWeAre' => 'pages#WhoWeAre'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
   # Defines the root path route ("/")
   # root "articles#index"
 end
