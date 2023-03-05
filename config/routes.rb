dc6eb1c6518ef93c93848bdf67e203e1deb402d9
Tamara DBre <breversemail@gmail.com>
2023-03-05 23:41:58 +0000

    WhoWeAre page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   devise_for :clients
   # Define the root for the website
   root 'pages#home'
+  get 'WhoWeAre', to: 'pages#WhoWeAre'
 
-  get 'WhoWeAre' => 'pages#WhoWeAre'
+  # Define the sign-out route for the Clients model
+  devise_scope :client do
+    delete '/clients/sign_out', to: 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  
   # Defines the root path route ("/")
