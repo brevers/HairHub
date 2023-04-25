ca4912736723be5918be2495e8c1ecef998068c4
Tamara DBre <breversemail@gmail.com>
2023-04-25 18:58:16 -0300

    remove original models

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :views
-  devise_for :agencies
-  devise_for :clients
-
   # Define the root for the website
   root 'pages#home'
+
   get 'who_we_are', to: 'pages#who_we_are'
   get 'how_it_works', to: 'pages#how_it_works'
   get 'agencies', to: 'pages#agencies'
   get 'clients', to: 'pages#clients'
   get 'find_agency', to: 'pages#find_agency'
 
-  # Define the sign-out route for the Clients model
-  devise_scope :client do
-    delete '/clients/sign_out', to: 'devise/sessions#destroy'
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_211232) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "agencies", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_agencies_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_agencies_on_reset_password_token", unique: true
-  end
-
-  create_table "clients", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_clients_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
-  end
-
-  create_table "views", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_views_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
-  end
 
 end
