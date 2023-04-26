70a99e8cfa065aaf731792b363416dea1468da09
Tamara DBre <breversemail@gmail.com>
2023-04-25 22:50:39 -0300

    Seed models under new schema

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,2 +1,5 @@
 class Agency < ApplicationRecord
+  belongs_to :user, optional: true
+
+  alias_attribute :owner, :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+User.find_or_create_by(email: "john@example.com") do |user|
+  user.name = "John Doe"
+  user.password = "123123"
+  user.password_confirmation = "123123"
+end
+
+jane = User.find_or_create_by(email: "jane@example.com") do |user|
+  user.name = "Jane Doe"
+  user.password = "123123"
+  user.password_confirmation = "123123"
+  user.owner!
+end
+
+grace = User.find_or_create_by(email: "grace@example.com") do |user|
+  user.name = "Grace Doe"
+  user.password = "123123"
+  user.password_confirmation = "123123"
+  user.owner!
+end
+
+Agency.find_or_create_by(name: "Jane's Co") do |agency|
+  agency.owner = jane
+  agency.pitch = "Best company in town"
+  agency.latitude = 53.350140
+  agency.longitude = -6.266155
+end
+
+Agency.find_or_create_by(name: "Grace's Co") do |agency|
+  agency.owner = grace
+  agency.pitch = "Far away but the better"
+  agency.latitude = 53.350140
+  agency.longitude = -6.266155
+end
